1aa6bdd80f64c82cdc8c56c3d445bb88a7ecbff1
Alex <alexis.autres@gmail.com>
2023-08-27 20:12:39 +0200

    Update typo url

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
+require(`dotenv`).config();
 
-const express = require('express');
+const express = require(`express`);
 const app = express();
 
+app.use(require(`./logger`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Set up CORS headers
-const cors = require('cors');
-app.use(cors({
-    origin: function(origin, callback) {
-        const allowedOrigins = [
-            'http://localhost:4200',
-            'http://www.alexgrz.vercel.app',
-            'https://www.alexgrz.vercel.app',
-            'https://alexgrz.vercel.app',
-        ];
-        
-        // Affichez l'origine pour déboguer
-        console.log("Received origin:", origin);
-        
+const cors = require(`cors`);
+const allowedOrigins = [
+    `http://localhost:3000`,    // Corrected typo here
+    `http://localhost:4200`,
+    `http://www.alexgrz.vercel.app`,
+    `https://www.alexgrz.vercel.app`,
+    `https://alexgrz.vercel.app`,
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
@@ -24,21 +26,14 @@ app.use(cors({
         }
     },
     credentials: true
-}));
+};
 
-app.use(require('./logger'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
-
-// app.use(cors({
-//     origin: '*',
-//     credentials: true
-// }));
 
 // Connect to MongoDB using mongoose
-const mongoose = require('mongoose');
-const config = require('./config');
+const mongoose = require(`mongoose`);
+const config = require(`./config`);
 const port = process.env.PORT || 3000;
 mongoose.connect(`${config.mongoURI}?tls=true&tlsInsecure=true`, {
   useNewUrlParser: true,
@@ -47,16 +42,16 @@ mongoose.connect(`${config.mongoURI}?tls=true&tlsInsecure=true`, {
 
 const db = mongoose.connection;
 const router = express.Router({mergeParams: true});
-const apiRouter = require('./router')(router);
+const apiRouter = require(`./router`)(router);
 
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.use('/v1', apiRouter);
+db.once(`open`, () => {
+    console.log(`Connected to MongoDB`);
+    app.use(`/v1`, apiRouter);
     app.listen(port, () => console.log(`Server is running on port ${port}`));
 });
 
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
+db.on(`error`, (err) => {
+    console.error(`MongoDB connection error:`, err);
 });
 
 module.exports = app;
