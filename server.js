1fb173b2ade0e37def2c482e6ecace34b62fbdd9
Alex <alexis.autres@gmail.com>
2023-11-15 17:20:21 +0100

    Update connectMongo

--- server.js
+++ server.js
@@ -1,42 +1,57 @@
-require(`dotenv`).config();
+require('dotenv').config();
 
-const express = require(`express`);
+const express = require('express');
 const app = express();
 
-app.use(require(`./logger`));
+app.use(require('./logger'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set up CORS headers
-const cors = require(`cors`);
+const cors = require('cors');
 app.use(cors({
     origin: '*',
     credentials: true
 }));
 
-
 // Connect to MongoDB using mongoose
-const mongoose = require(`mongoose`);
-const config = require(`./config`);
+const mongoose = require('mongoose');
+const config = require('./config');
 const port = process.env.PORT || 3000;
+let dbConnection = null;
 
-mongoose.connect(`${config.mongoURI}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Function to establish or return existing MongoDB connection
+function connectToMongo() {
+    const connectionOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
 
-const db = mongoose.connection;
-const router = express.Router({mergeParams: true});
-const apiRouter = require(`./router`)(router);
+    if (!dbConnection || dbConnection.readyState !== 1) {
+        console.log('No db init > mongoose connect');
+        mongoose.connect(`${config.mongoURI}`, connectionOptions);
+        dbConnection = mongoose.connection; // Store the connection
+        return dbConnection;
+    } else {
+        console.log('db already init');
+        return dbConnection;
+    }
+}
 
-db.once(`open`, () => {
-    console.log(`Connected to MongoDB`);
-    app.use(`/v1`, apiRouter);
+const router = express.Router({ mergeParams: true });
+const apiRouter = require('./router')(router);
+
+// Use const for db variable
+dbConnection = connectToMongo();
+
+dbConnection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.use('/v1', apiRouter);
     app.listen(port, () => console.log(`Server is running on port ${port}`));
 });
 
-db.on(`error`, (err) => {
-    console.error(`MongoDB connection error:`, err);
+dbConnection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
 });
 
 module.exports = app;
