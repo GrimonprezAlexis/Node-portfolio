f55f9f991dc78524a65a937db472bff25dc87737
Alex <alexis.autres@gmail.com>
2023-12-06 21:12:52 +0100

    Update Mongo DB SIGNINT

--- server.js
+++ server.js
@@ -1,57 +1,66 @@
+// Load environment variables
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI || 'your_default_mongo_uri';
 
 app.use(require('./logger'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set up CORS headers
-const cors = require('cors');
 app.use(cors({
     origin: '*',
     credentials: true
 }));
 
 // Connect to MongoDB using mongoose
-const mongoose = require('mongoose');
-const config = require('./config');
-const port = process.env.PORT || 3000;
-let dbConnection = null;
+async function connectToMongo() {
+    try {
+        const connectionOptions = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
 
-// Function to establish or return existing MongoDB connection
-function connectToMongo() {
-    const connectionOptions = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
-
-    if (!dbConnection || dbConnection.readyState !== 1) {
-        console.log('No db init > mongoose connect');
-        mongoose.connect(`${config.mongoURI}`, connectionOptions);
-        dbConnection = mongoose.connection; // Store the connection
-        return dbConnection;
-    } else {
-        console.log('db already init');
-        return dbConnection;
+        await mongoose.connect(mongoURI, connectionOptions);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit the process on connection error
     }
 }
 
+// Function to handle SIGINT signal
+function handleSIGINT() {
+    mongoose.connection.close(() => {
+        console.log('MongoDB connection closed');
+        process.exit(0); // Exit the process after closing MongoDB connection
+    });
+}
+
+// Use const for constants
 const router = express.Router({ mergeParams: true });
 const apiRouter = require('./router')(router);
 
-// Use const for db variable
-dbConnection = connectToMongo();
+// Use async/await for better readability
+(async () => {
+    await connectToMongo();
 
-dbConnection.once('open', () => {
-    console.log('Connected to MongoDB');
+    // Handle SIGINT signal for graceful shutdown
+    process.on('SIGINT', handleSIGINT);
+
     app.use('/v1', apiRouter);
+
+    // Add generic error handling middleware
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something went wrong!');
+    });
+
     app.listen(port, () => console.log(`Server is running on port ${port}`));
-});
-
-dbConnection.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
-
-module.exports = app;
+})();
